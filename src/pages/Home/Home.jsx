2c4737a89afc6179531fe63636e21994391efc06
Elie <97699616+Elie-Khoury@users.noreply.github.com>
2024-06-07 23:54:10 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header.jsx";
 import HeroCarousel from "./components/HeroCarousel.jsx";
 import SportBox from "./components/SportBox.jsx";
 import CarouselSection from "./components/CarouselSection.jsx";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "../../global-Components/Navbar.jsx";
 
 function Home(){
 
